ab4a5618c494ced6d3db3a14054eaf92a2bb7ad1
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-03-26 02:48:47 -0400

    WIP: Exempli: All but complex testing (and whatever it uncovers)

--- groovy/json/JsonConjoin.groovy
+++ groovy/json/JsonConjoin.groovy
@@ -41,16 +41,26 @@ class JsonConjoin implements Serializable
 
         if (base  == null)
         {
-            Boolean underIsProto = exempli in List
-            Object  rval         = underIsProto ? null : deepCopy(exempli)
+            switch (exempli)
+            {
+                case List:     return null
+                case Exempli:  return exempli.whenNull()
 
-            return rval
+                default:       return deepCopy(exempli)
+            }
         }
 
         if ((base in Number) && (exempli in Number)) return base
 
         if ((base in Map ) && (exempli in Map )) return applyMap (exempli)
         if ((base in List) && (exempli in List)) return applyList(exempli)
+
+        if (exempli in Exempli)
+        {
+            if (base in List) return exempli.applyTo(base)
+
+            throw new IllegalArgumentException("Exempli class may only be applied to List class instances")
+        }
 
         if (base in exempli.getClass()) return deepCopy(base)
 
@@ -64,10 +74,31 @@ class JsonConjoin implements Serializable
 
     private Map applyMap(Map exempli)
     {    
-        Map filteredDefaults    = MapX.collectEntries (exempli) { k, v -> base.containsKey(k) ? null : [(k): deepCopy(v) ] }
-        Map recursiveBaseValues = MapX.collectEntries (base   ) { k, v -> [(k) : recurse(base[k], exempli[k])] }
+        Map filteredExempli    = MapX.collectEntries (exempli) 
+        { k, v ->
+            if (base.containsKey(k)) return null
 
-        return filteredDefaults << recursiveBaseValues
+            if (v in Exempli)
+            {
+                Object whenNull = v.whenNull()
+
+                return whenNull == null ? null : [(k): whenNull]
+            }
+
+            return [(k): deepCopy(v)]
+        }
+
+        Map recursiveBaseValues = MapX.collectEntries (base)
+        { k, v ->
+            def e = exempli[k]
+
+            (e in Exempli
+                ? [(k) : e.applyTo(v)] 
+                : [(k) : recurse(v, e)]
+            )
+        }
+
+        return filteredExempli << recursiveBaseValues
     }
 
     private List applyList(List exempli)
@@ -110,6 +141,10 @@ class JsonConjoin implements Serializable
                 List rval = ListX.collect(item) { it -> [deepCopy(it)] }
                 return rval
 
+            case Exempli:
+
+                throw new IllegalArgumentException("Exempli instance should not be copied directly")
+
             default:
 
                 throw new IllegalArgumentException("Don't know how to handle class: ${item.getClass()}")
@@ -131,20 +166,44 @@ class JsonConjoin implements Serializable
     {
         final List prepend
         final List append
-        final List ifNothing
-        final List subExempli
+        final List ifEmpty
+        final List apply
 
         Exempli(Map args = [:])
         {
-            this.prepend    = args.prepend    ? args.prepend    : []
-            this.append     = args.append     ? args.append     : []
-            this.ifNothing  = args.ifNothing  ? args.ifNothing  : []
-            this.subExempli = args.subExempli ? args.subExempli : []
+            this.prepend = args.prepend ? args.prepend : []
+            this.append  = args.append  ? args.append  : []
+            this.ifEmpty = args.ifEmpty ? args.ifEmpty : []
+            this.apply   = args.apply   ? args.apply   : []
         }
 
+        List applyTo(List base)
+        {
+            List rval = []
+            
+            rval.addAll(this.prepend)
+            rval.addAll(base.isEmpty() ? this.ifEmpty : base)
+            rval.addAll(this.append)
+
+            if (apply)
+            {
+                rval = ListX.collect(rval)
+                { r -> 
+                    apply.inject(r) { v, x -> [JsonConjoin.of(v, x)] }
+                }
+            }
+
+            rval
+        }
+        
+        List whenNull()
+        {
+            (this.ifEmpty) ? applyTo([]) : []
+        }
+        
         String toString()
         {
-            new JsonBuilder(this).toPrettyString()
+            new JsonBuilder(this).toString()
         }
     }
 }
--- groovy/json/JsonConjoinTest.groovy
+++ groovy/json/JsonConjoinTest.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonBuilder
 import com.mukadobo.zpers.groovy.json.JsonConjoin
 
 Map  mapE  = [:]
@@ -233,42 +234,293 @@ println()
 
 //     ['asK2L2   ', 'asJ2L1   ', [jj:'J2', kk:'K2', ll:'L2'] ],
 
-def base    = aaMaps['asK2L2']
-def exempli = aaMaps['asJ2L1']
-def expect  = [jj:'J2', kk:'K2', ll:'L2']
-
-base    = [ 'itemsq' : [50         ] ]
-exempli = [ 
-    'items' : JsonConjoin.exempli(
-        'prepend' : [20,21], 
-        'append': [30,31,32], 
-        'ifEmtpy': [90,91], 
-        'apply': [
-            [xx:'X1',yy:'Y1'],
-            [xx:'X2',zz:'Z2']
-        ]
-    )
-]
-expect  = [ 'items' : [10,11,12,50] ]
-
-
 def Object doit(base, exempli) 
 {
     println()
-    println("base=$base")
-    println("exempli=$exempli")
+    println("base=${new JsonBuilder(base).toPrettyString()}")
+    println("exempli=${new JsonBuilder(exempli).toPrettyString()}")
 
     def rval = JsonConjoin.basedOn(base).apply(exempli)
-    println("rval=$rval")
+    println("rval=${new JsonBuilder(rval).toPrettyString()}")
 
     rval
 }
 
-assert doit(base, exempli) == expect
+def Map ccJCE = [
+    'Peas' : JsonConjoin.exempli(
+        'prepend' : [[aa:'A1'], [bb:'B1']], 
+    ),
+    'pEas' : JsonConjoin.exempli(
+        'ifEmpty' : [[jj:'J1'], [kk:'K1']], 
+    ),
+    'peAs' : JsonConjoin.exempli(
+        'append'  : [[rr:'R1'], [ss:'S1']], 
+    ),
+    'PEAs' : JsonConjoin.exempli(
+        'prepend' : [[aa:'A1'], [bb:'B1']], 
+        'ifEmpty' : [[jj:'J1'], [kk:'K1']], 
+        'append'  : [[rr:'R1'], [ss:'S1']], 
+    ),
 
-// $pre  [<]   [pre]    items<
-// $app  [>]   [app]    items>
-// $if0  [*]   [if0]    items*
-// $exm  [=]   [exm]    items~
+    'peaS' : JsonConjoin.exempli(
+        'apply'   : [
+            [jj:'J2', ll:'L2'],
+        ]
+    ),
+
+    'PEAS' : JsonConjoin.exempli(
+        'prepend' : [[aa:'A1'], [bb:'B1']], 
+        'ifEmpty' : [[jj:'J1'], [kk:'K1']], 
+        'append'  : [[rr:'R1'], [ss:'S1']], 
+        'apply'   : [
+            [aa:'A2', cc:'C2'],
+            [jj:'J2', ll:'L2'],
+            [rr:'R2', tt:'T2'],
+        ]
+    ),
+]
+def Map ccExempli = [ 
+    ('extant_'+'Peas') : [ 'extant' : ccJCE.Peas ], ('nosuch_'+'Peas') : [ 'nosuch' : ccJCE.Peas ], 
+    ('extant_'+'pEas') : [ 'extant' : ccJCE.pEas ], ('nosuch_'+'pEas') : [ 'nosuch' : ccJCE.pEas ], 
+    ('extant_'+'peAs') : [ 'extant' : ccJCE.peAs ], ('nosuch_'+'peAs') : [ 'nosuch' : ccJCE.peAs ], 
+    ('extant_'+'PEAs') : [ 'extant' : ccJCE.PEAs ], ('nosuch_'+'PEAs') : [ 'nosuch' : ccJCE.PEAs ], 
+
+    ('extant_'+'peaS') : [ 'extant' : ccJCE.peaS ], ('nosuch_'+'peaS') : [ 'nosuch' : ccJCE.peaS ], 
+    ('extant_'+'PEAS') : [ 'extant' : ccJCE.PEAS ], ('nosuch_'+'PEAS') : [ 'nosuch' : ccJCE.PEAS ], 
+]
+
+def Map ccBase = [
+    'simple' : [ 
+        'extant' : [ 
+            [jj:'J0'], 
+            [kk:'K0'], 
+        ],
+    ],
+    'others' : [ 
+        'others' : [ 
+            [yy:'Y0'], 
+        ],
+    ],
+    'monty' : [ 
+        'extant' : [ 
+            [jj:'J0'], 
+            [kk:'K0'],
+            [   aa:'A0', bb:'B0', cc:'C0',
+                jj:'J0', kk:'K0', ll:'L0',
+                rr:'R0', ss:'S0', tt:'T0',
+                xx:'X0',
+            ],
+        ],
+        'others' : [ 
+            [yy:'Y0'], 
+        ],
+    ],
+]
+
+def expect  = [ 
+    'extant' : [
+        [aa:'A1',          cc:'C2',     jj:'J2',          ll:'L2',     rr:'R2',          tt:'T2'], 
+        [aa:'A2', bb:'B1', cc:'C2',     jj:'J2',          ll:'L2',     rr:'R2',          tt:'T2'], 
+
+        [aa:'A2',          cc:'C2',     jj:'J0',          ll:'L2',     rr:'R2',          tt:'T2'], 
+        [aa:'A2',          cc:'C2',     jj:'J2', kk:'K0', ll:'L2',     rr:'R2',          tt:'T2'], 
+
+        [aa:'A2',          cc:'C2',     jj:'J2',          ll:'L2',     rr:'R1',          tt:'T2'], 
+        [aa:'A2',          cc:'C2',     jj:'J2',          ll:'L2',     rr:'R2', ss:'S1', tt:'T2'], 
+
+        [aa:'A0', bb:'B0', cc:'C0',     jj:'J0', kk:'K0', ll:'L0',     rr:'R0', ss:'S0', tt:'T0',   xx:'X0' ],
+    ],
+    'nosuch' : [
+        [aa:'A1',          cc:'C2',     jj:'J2',          ll:'L2',     rr:'R2',          tt:'T2'], 
+        [aa:'A2', bb:'B1', cc:'C2',     jj:'J2',          ll:'L2',     rr:'R2',          tt:'T2'], 
+
+        [aa:'A2',          cc:'C2',     jj:'J1',          ll:'L2',     rr:'R2',          tt:'T2'], 
+        [aa:'A2',          cc:'C2',     jj:'J2', kk:'K1', ll:'L2',     rr:'R2',          tt:'T2'], 
+
+        [aa:'A2',          cc:'C2',     jj:'J2',          ll:'L2',     rr:'R1',          tt:'T2'], 
+        [aa:'A2',          cc:'C2',     jj:'J2',          ll:'L2',     rr:'R2', ss:'S1', tt:'T2'], 
+    ]
+]
+
+List lrcCCExempli = [
+    ['simple', ('extant_'+'Peas'), [
+        'extant': [
+            [aa:'A1', ],
+            [bb:'B1', ],
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+        ],
+    ]],
+    ['simple', ('nosuch_'+'Peas'), [
+        'nosuch': [ ],
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+        ],
+    ]],
+    ['others', ('nosuch_'+'Peas'), [
+        'nosuch': [ ],
+        'others': [
+            [yy:'Y0', ], 
+        ],
+    ]],
+    //------------------------------------------
+    ['simple', ('extant_'+'pEas'), [
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ],  
+        ],
+    ]],
+    ['simple', ('nosuch_'+'pEas'), [
+        'nosuch': [ 
+            [jj:'J1', ], 
+            [kk:'K1', ], 
+        ],
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+        ],
+    ]],
+    ['others', ('nosuch_'+'pEas'), [
+        'nosuch': [
+            [jj:'J1', ], 
+            [kk:'K1', ], 
+        ],
+        'others': [
+            [yy:'Y0', ], 
+        ],
+    ]],
+    //------------------------------------------
+    ['simple', ('extant_'+'peAs'), [
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+            [rr:'R1', ],
+            [ss:'S1', ],
+        ],
+    ]],
+    ['simple', ('nosuch_'+'peAs'), [
+        'nosuch': [ ],
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+        ],
+    ]],
+    ['others', ('nosuch_'+'peAs'), [
+        'nosuch': [ ],
+        'others': [
+            [yy:'Y0', ], 
+        ],
+    ]],
+    //--------------------- ---------------------
+    ['simple', ('extant_'+'PEAs'), [
+        'extant': [
+            [aa:'A1', ],
+            [bb:'B1', ],
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+            [rr:'R1', ],
+            [ss:'S1', ],
+        ],
+    ]],
+    ['simple', ('nosuch_'+'PEAs'), [
+        'nosuch': [ 
+            [aa:'A1', ],
+            [bb:'B1', ],
+            [jj:'J1', ], 
+            [kk:'K1', ], 
+            [rr:'R1', ],
+            [ss:'S1', ],
+        ],
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+        ],
+    ]],
+    ['others', ('nosuch_'+'PEAs'), [
+        'nosuch': [ 
+            [aa:'A1', ],
+            [bb:'B1', ],
+            [jj:'J1', ],  
+ 
+ 
+ 
+ 
+ 
+ 
+            [kk:'K1', ], 
+            [rr:'R1', ],
+            [ss:'S1', ],
+        ],
+        'others': [
+            [yy:'Y0', ], 
+        ],
+    ]],
+    //------------------------------------------
+//  'peaS' : JsonConjoin.exempli(
+//      'apply'   : [
+//          [jj:'J2', ll:'L2'],
+//      ]
+//  ),
+
+    ['simple', ('extant_'+'peaS'), [
+        'extant': [
+            [jj:'J0',          ll:'L2', ],
+            [jj:'J2', kk:'K0', ll:'L2', ],
+       ],
+    ]],
+    ['simple', ('nosuch_'+'peaS'), [
+        'nosuch': [ ],
+        'extant': [
+            [jj:'J0', ], 
+            [kk:'K0', ], 
+        ],
+    ]],
+    ['others', ('nosuch_'+'peaS'), [
+        'nosuch': [ ],
+        'others': [
+            [yy:'Y0', ], 
+        ],
+    ]],
+]
+
+lrcCCExempli.each
+{ spec ->
+    String nmLeft    = spec[0].trim(); Object itLeft =ccBase   [nmLeft ]
+    String nmRight   = spec[1].trim(); Object itRight=ccExempli[nmRight] 
+    Object expected  = spec[2]
+
+    println ("lrcCCExempli: L<$nmLeft>=$itLeft  -|- R<$nmRight>=$itRight  => ${expected}")
+
+    assert itLeft != null
+    assert itRight != null
+
+    Object    result = null
+    Exception caught = null
+    try
+    {
+        result = doit(itLeft, itRight)
+    }
+    catch (e)
+    {
+        caught = e
+    }
+    println ("lrcCCExempli: L<$nmLeft>  -|- R<$nmRight>\n")
+
+    if (expected in Exception)
+    {
+        assert caught
+        assert caught in expected
+    }
+    else
+    {
+        if (caught) // caught.printStackTrace(System.out)
+            org.codehaus.groovy.runtime.StackTraceUtils.sanitize(new Exception(caught)).printStackTrace()
+
+        assert !caught
+        assert result == expected
+    }
+}
+println()
 
 /**/
